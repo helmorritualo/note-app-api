37293721f8acc518a614c0b3d42ffd671b52e492
Helmor Ritualo <helmorritualo@gmail.com>
2025-06-24 14:56:57 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { Context, Next } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
+import { jwt } from "hono/jwt";
+import { PORT, JWT_SECRET } from "./config/env";
+import connectionToDatabase from "./config/database";
+
+const app = new Hono();
+
+// middlewares
+app.use("*", logger());
+app.use(secureHeaders());
+app.use(
+  "*",
+  cors({
+    origin: ["*"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    credentials: true,
+    maxAge: 600,
+  }),
+);
+
+app.use("/api/v1/*", async (c: Context, next: Next) => {
+  const path = c.req.path;
+
+  //* Skip auth routes
+  if (path === "/api/v1/auth/login" || path === "/api/v1/auth/register") {
+    return next();
+  }
+
+  return jwt({
+    secret: JWT_SECRET as string,
+  })(c, next);
+});
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+connectionToDatabase().then(() => {
+  serve(
+    {
+      fetch: app.fetch,
+      port: Number(PORT),
+    },
+    (info) => {
+      console.log(`Server is running on http://localhost:${info.port}`);
+    },
+  );
+});
