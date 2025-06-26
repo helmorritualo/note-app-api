c53b3f7255d5d0f550615066afb2923d4473a3cc
Helmor Ritualo <helmorritualo@gmail.com>
2025-06-26 10:29:25 +0800

    feat: implement routes

---
+++ src/controllers/routes.ts
@@ -0,0 +1,5 @@
+export const routes = [
+	// routes
+];
+
+export type AppRoute = (typeof routes)[number];
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { secureHeaders } from "hono/secure-headers";
 import { jwt } from "hono/jwt";
 import { PORT, JWT_SECRET } from "./config/env";
 import connectionToDatabase from "./config/database";
+import { routes } from "./controllers/routes";
 
 const app = new Hono();
 
@@ -14,42 +15,42 @@ const app = new Hono();
 app.use("*", logger());
 app.use(secureHeaders());
 app.use(
-  "*",
-  cors({
-    origin: ["*"],
-    allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowHeaders: ["Content-Type", "Authorization"],
-    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
-    credentials: true,
-    maxAge: 600,
-  }),
+	"*",
+	cors({
+		origin: ["*"],
+		allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+		allowHeaders: ["Content-Type", "Authorization"],
+		exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+		credentials: true,
+		maxAge: 600,
+	}),
 );
 
 app.use("/api/v1/*", async (c: Context, next: Next) => {
-  const path = c.req.path;
+	const path = c.req.path;
 
-  //* Skip auth routes
-  if (path === "/api/v1/auth/login" || path === "/api/v1/auth/register") {
-    return next();
-  }
+	//* Skip auth routes
+	if (path === "/api/v1/auth/login" || path === "/api/v1/auth/register") {
+		return next();
+	}
 
-  return jwt({
-    secret: JWT_SECRET as string,
-  })(c, next);
+	return jwt({
+		secret: JWT_SECRET as string,
+	})(c, next);
 });
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+routes.forEach((route) => {
+	app.route("/api/v1", route);
 });
 
 connectionToDatabase().then(() => {
-  serve(
-    {
-      fetch: app.fetch,
-      port: Number(PORT),
-    },
-    (info) => {
-      console.log(`Server is running on http://localhost:${info.port}`);
-    },
-  );
+	serve(
+		{
+			fetch: app.fetch,
+			port: Number(PORT),
+		},
+		(info) => {
+			console.log(`Server is running on http://localhost:${info.port}`);
+		},
+	);
 });
